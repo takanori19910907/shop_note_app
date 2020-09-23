89cb18c592ac5220440c76537d0db32f057fae05
有吉　貴紀 <>
2020-09-23 21:20:00 +0900

    favorite/index,newアクションのリファクタリング

--- app/controllers/favorite_items_controller.rb
+++ app/controllers/favorite_items_controller.rb
@@ -1,90 +1,14 @@
 class FavoriteItemsController < ApplicationController
   before_action :authenticate_user!
   before_action :correct_user, only: [:destroy]
+  before_action :set_items, only: [:index,:new]
+  before_action :sort_items, only: [:index,:new]
 
   def index
-    @favorite_items = current_user.favorite_items.all
-    @A_line = []
-    @K_line = []
-    @S_line = []
-    @T_line = []
-    @N_line = []
-    @H_line = []
-    @M_line = []
-    @Y_line = []
-    @R_line = []
-    @W_line = []
-    @other_line = []
-
-    @favorite_items.each do |item|
-      i_initial = NKF.nkf('-h1 -w', item.name[0]).tr('A-Z0-9', 'Ａ-Ｚ０-９')
-      if %w[あ い う え お a i u e o A I U E O].include?(i_initial)
-        @A_line << item
-      elsif %w[か き く け こ が ぎ ぐ げ ご c k q C K Q].include?(i_initial)
-        @K_line << item
-      elsif %w[さ し す せ そ ざ じ ず ぜ ぞ s z S Z].include?(i_initial)
-        @S_line << item
-      elsif %w[た ち つ て と だ ぢ づ で ど t d T D].include?(i_initial)
-        @T_line << item
-      elsif %w[な に ぬ ね の n N].include?(i_initial)
-        @N_line << item
-      elsif %w[は ひ ふ へ ほ ば び ぶ べ ぼ h b H B].include?(i_initial)
-        @H_line << item
-      elsif %w[ま み む め も m M].include?(i_initial)
-        @M_line << item
-      elsif %w[や ゆ よ y Y].include?(i_initial)
-        @Y_line << item
-      elsif %w[ら り る れ ろ l r L R].include?(i_initial)
-        @R_line << item
-      elsif %w[わ を ん].include?(i_initial)
-        @W_line << item
-      else
-        @other_line << item
-      end
-    end
   end
 
   def new
     @favorite_item = current_user.favorite_items.new
-    @favorite_items = current_user.favorite_items.all
-    @A_line = []
-    @K_line = []
-    @S_line = []
-    @T_line = []
-    @N_line = []
-    @H_line = []
-    @M_line = []
-    @Y_line = []
-    @R_line = []
-    @W_line = []
-    @other_line = []
-
-    @favorite_items.each do |item|
-      i_initial = NKF.nkf('-h1 -w', item.name[0]).tr('A-Z0-9', 'Ａ-Ｚ０-９')
-      if %w[あ い う え お a i u e o A I U E O].include?(i_initial)
-        @A_line << item
-      elsif %w[か き く け こ が ぎ ぐ げ ご c k q C K Q].include?(i_initial)
-        @K_line << item
-      elsif %w[さ し す せ そ ざ じ ず ぜ ぞ s z S Z].include?(i_initial)
-        @S_line << item
-      elsif %w[た ち つ て と だ ぢ づ で ど t d T D].include?(i_initial)
-        @T_line << item
-      elsif %w[な に ぬ ね の n N].include?(i_initial)
-        @N_line << item
-      elsif %w[は ひ ふ へ ほ ば び ぶ べ ぼ h b H B].include?(i_initial)
-        @H_line << item
-      elsif %w[ま み む め も m M].include?(i_initial)
-        @M_line << item
-      elsif %w[や ゆ よ y Y].include?(i_initial)
-        @Y_line << item
-      elsif %w[ら り る れ ろ l r L R].include?(i_initial)
-        @R_line << item
-      elsif %w[わ を ん].include?(i_initial)
-        @W_line << item
-      else
-        @other_line << item
-      end
-    end
   end
 
   def create
@@ -138,11 +62,57 @@ class FavoriteItemsController < ApplicationController
 
   private
 
-  def correct_user
-    item = FavoriteItem.find(params[:item][:id]).first
-    unless item.user_id == current_user.id
-      flash[:danger] = '投稿者本人でないため削除出来ませんでした'
-      redirect_to request.referrer || root_url
+    def correct_user
+      item = FavoriteItem.find(params[:item][:id]).first
+      unless item.user_id == current_user.id
+        flash[:danger] = '投稿者本人でないため削除出来ませんでした'
+        redirect_to request.referrer || root_url
+      end
     end
-  end
+
+    def set_items
+      @favorite_items = current_user.favorite_items.all
+    end
+
+    def sort_items
+      @A_line = []
+      @K_line = []
+      @S_line = []
+      @T_line = []
+      @N_line = []
+      @H_line = []
+      @M_line = []
+      @Y_line = []
+      @R_line = []
+      @W_line = []
+      @other_line = []
+      @favorite_items.each do |item|
+      item_initial = NKF.nkf('-h1 -w', item.name[0]).tr('Ａ-Ｚ０-９','A-Z0-9').downcase
+
+      case item_initial
+      when "あ","い","う","え","お","a","i","u","e","o"
+        @A_line << item
+      when "か","き","く","け","こ","が","ぎ","ぐ","げ","ご","c","k","q"
+        @K_line << item
+      when "さ","し","す","せ","そ","ざ","じ","ず","ぜ","ぞ","s","z"
+        @S_line << item
+      when "た","ち","つ","て","と","だ","ぢ","づ","で","ど","t","d"
+        @T_line << item
+      when "な","に","ぬ","ね","の","n"
+        @N_line << item
+      when "は","ひ","ふ","へ","ほ","ば","び","ぶ","べ","ぼ","ぱ","ぴ","ぷ","ぺ","ぽ","h","b","p"
+        @H_line << item
+      when "ま","み","む","め","も","m"
+        @M_line << item
+      when "や","ゆ","よ","y"
+        @Y_line << item
+      when "ら","り","る","れ","ろ","r","l"
+        @R_line << item
+      when "わ","を","ん","w"
+        @W_line << item
+      else
+        @other_line << item
+      end
+      end
+    end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @own_notes = if current_user.present?
-                   current_user.notes.includes(:comments).all
+                   current_user.notes.includes(:comments)
                  else
                    []
                  end
@@ -21,7 +21,7 @@ class HomeController < ApplicationController
 
   def tutorial_note
     @own_notes = if current_user.notes.present?
-                   current_user.notes.all
+                   current_user.notes
                  else
                    []
                  end
@@ -41,32 +41,68 @@ class HomeController < ApplicationController
     @R_line = []
     @W_line = []
     @other_line = []
-
     @favorite_items.each do |item|
-      i_initial = NKF.nkf('-h1 -w', item.name[0]).tr('A-Z0-9', 'Ａ-Ｚ０-９')
-      if %w[あ い う え お a i u e o A I U E O].include?(i_initial)
+      # i_initial = NKF.nkf('-h1 -w', item.name[0]).tr('A-Z0-9', 'Ａ-Ｚ０-９')
+      # case i_initial
+      # when "あ","い","う","え","お","a","i","u","e","o","A","I","U","E","O",
+      #   @A_line << item
+      # when "か","き","く","け","こ","が","ぎ","ぐ","げ","ご","c","k","q","C","K","Q",
+      #   @K_line << item
+      # else
+      #   @other_line << item
+      # end
+      initial = NKF.nkf('-h1 -w', item.name[0]).tr('Ａ-Ｚ０-９','A-Z0-9').downcase
+
+      case initial
+      when "あ","い","う","え","お","a","i","u","e","o"
         @A_line << item
-      elsif %w[か き く け こ が ぎ ぐ げ ご c k q C K Q].include?(i_initial)
+      when "か","き","く","け","こ","が","ぎ","ぐ","げ","ご","c","k","q"
         @K_line << item
-      elsif %w[さ し す せ そ ざ じ ず ぜ ぞ s z S Z].include?(i_initial)
+      when "さ","し","す","せ","そ","ざ","じ","ず","ぜ","ぞ","s","z"
         @S_line << item
-      elsif %w[た ち つ て と だ ぢ づ で ど t d T D].include?(i_initial)
+      when "た","ち","つ","て","と","だ","ぢ","づ","で","ど","t","d"
         @T_line << item
-      elsif %w[な に ぬ ね の n N].include?(i_initial)
+      when "な","に","ぬ","ね","の","n"
         @N_line << item
-      elsif %w[は ひ ふ へ ほ ば び ぶ べ ぼ h b H B].include?(i_initial)
+      when "は","ひ","ふ","へ","ほ","ば","び","ぶ","べ","ぼ","ぱ","ぴ","ぷ","ぺ","ぽ","h","b","p"
         @H_line << item
-      elsif %w[ま み む め も m M].include?(i_initial)
+      when "ま","み","む","め","も","m"
         @M_line << item
-      elsif %w[や ゆ よ y Y].include?(i_initial)
+      when "や","ゆ","よ","y"
         @Y_line << item
-      elsif %w[ら り る れ ろ l r L R].include?(i_initial)
+      when "ら","り","る","れ","ろ","r","l"
         @R_line << item
-      elsif %w[わ を ん].include?(i_initial)
+      when "わ","を","ん","w"
         @W_line << item
       else
         @other_line << item
       end
+
+
+      # if [あ い う え お a i u e o].include?(i_initial)
+      #   @A_line << item
+      # elsif %w[か き く け こ が ぎ ぐ げ ご c k q C K Q].include?(i_initial)
+      #   @K_line << item
+      # elsif %w[さ し す せ そ ざ じ ず ぜ ぞ s z S Z].include?(i_initial)
+      #   @S_line << item
+      # elsif %w[た ち つ て と だ ぢ づ で ど t d T D].include?(i_initial)
+      #   @T_line << item
+      # elsif %w[な に ぬ ね の n N].include?(i_initial)
+      #   @N_line << item
+      # elsif %w[は ひ ふ へ ほ ば び ぶ べ ぼ h b H B].include?(i_initial)
+      #   @H_line << item
+      # elsif %w[ま み む め も m M].include?(i_initial)
+      #   @M_line << item
+      # elsif %w[や ゆ よ y Y].include?(i_initial)
+      #   @Y_line << item
+      # elsif %w[ら り る れ ろ l r L R].include?(i_initial)
+      #   @R_line << item
+      # elsif %w[わ を ん].include?(i_initial)
+      #   @W_line << item
+      # else
+      #   @other_line << item
+      # end
+
     end
   end
 
