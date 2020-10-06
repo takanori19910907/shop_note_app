5572f553f2786c84b2c08b61ccbc6c2dbf0bdb09
有吉　貴紀 <>
2020-10-06 09:56:23 +0900

    自分自身のcommentを呼び出すためown_commentを定義

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,6 +2,7 @@ class Note < ApplicationRecord
   belongs_to :user
   belongs_to :group, optional: true
   has_many :comments, dependent: :destroy
+  has_many :own_comments, class_name: 'Comment', foreign_key: :note_id
 
   validates :user_id, presence: true
   validates :content, presence: true, unless: :content_blank?, length: { maximum: 30 }
