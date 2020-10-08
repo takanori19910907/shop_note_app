c75306c5e53371cb2d9168ba9a433b4879dfcbec
有吉　貴紀 <>
2020-10-08 19:53:03 +0900

    reviewsモデルのcontentカラムに制約追加

--- db/migrate/20200824084012_create_reviews.rb
+++ db/migrate/20200824084012_create_reviews.rb
@@ -3,7 +3,7 @@ class CreateReviews < ActiveRecord::Migration[5.1]
     create_table :reviews do |t|
       t.references :user, foreign_key: true
       t.string :title
-      t.text :content
+      t.text :content, null: false
       t.integer :rate
       t.timestamps
     end
