0f1c27e45441c91c720aa4b22618eeae675560bc
有吉　貴紀 <>
2020-10-23 23:52:39 +0900

    fat controllerの改善のため処理を削減

--- app/controllers/favorite_items_controller.rb
+++ app/controllers/favorite_items_controller.rb
@@ -2,7 +2,6 @@ class FavoriteItemsController < ApplicationController
   before_action :authenticate_user!
   before_action :correct_user, only: [:destroy]
   before_action :set_items, only: [:index,:new]
-  before_action :sort_items, only: [:index,:new]
 
   def index
   end
@@ -66,49 +65,9 @@ class FavoriteItemsController < ApplicationController
     end
 
     def set_items
-      @favorite_items = current_user.favorite_items.all
-    end
-
-    def sort_items
-      @A_line = []
-      @K_line = []
-      @S_line = []
-      @T_line = []
-      @N_line = []
-      @H_line = []
-      @M_line = []
-      @Y_line = []
-      @R_line = []
-      @W_line = []
-      @other_line = []
-      @favorite_items.each do |item|
-      item_initial = NKF.nkf('-h1 -w', item.name[0]).tr('Ａ-Ｚ０-９','A-Z0-9').downcase
-
-      case item_initial
-      when "あ","い","う","え","お","a","i","u","e","o"
-        @A_line << item
-      when "か","き","く","け","こ","が","ぎ","ぐ","げ","ご","c","k","q"
-        @K_line << item
-      when "さ","し","す","せ","そ","ざ","じ","ず","ぜ","ぞ","s","z"
-        @S_line << item
-      when "た","ち","つ","て","と","だ","ぢ","づ","で","ど","t","d"
-        @T_line << item
-      when "な","に","ぬ","ね","の","n"
-        @N_line << item
-      when "は","ひ","ふ","へ","ほ","ば","び","ぶ","べ","ぼ","ぱ","ぴ","ぷ","ぺ","ぽ","h","b","p"
-        @H_line << item
-      when "ま","み","む","め","も","m"
-        @M_line << item
-      when "や","ゆ","よ","y"
-        @Y_line << item
-      when "ら","り","る","れ","ろ","r","l"
-        @R_line << item
-      when "わ","を","ん","w"
-        @W_line << item
-      else
-        @other_line << item
-      end
-      end
+      favorite_items = current_user.favorite_items.all
+      @item_exists = favorite_items.present?
+      @favorite_item_groups = current_user.favorite_item_groups
     end
 
     def f_item_params
