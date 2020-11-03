8f4a01747eda66c7528077206a1bdf912a60c59d
有吉　貴紀 <>
2020-11-03 23:06:09 +0900

    ajaxによる投稿機能実装完了(個人投稿ページ) #21

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,20 +1,33 @@
 class NotesController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    if current_user.present?
+      @own_notes = current_user.notes.includes(comments: :user)
+      @note = current_user.notes.build
+    else
+      @own_notes = []
+    end
+  end
+
   def create
     note = current_user.notes.build(note_params)
-      if note.save
-        url = Rails.application.routes.recognize_path(request.referrer)
-        if url == {controller: 'home', action: 't_post'}
-          flash[:success] = '投稿に成功しました！'
-          redirect_to t_post_path
-        else
-          redirect_to request.referrer || root_url
-        end
-      else
-        flash[:danger] = '投稿に失敗しました'
-        redirect_to request.referrer || root_url
-      end
+      note.save
+      @own_notes = current_user.notes.includes(comments: :user)
+      # @group_notes = Note.includes(comments: :user).includes(:user).where(group_id: @group.id)
+    #     url = Rails.application.routes.recognize_path(request.referrer)
+    #     if url == {controller: 'home', action: 't_post'}
+    #       flash[:success] = '投稿に成功しました！'
+    #       redirect_to t_post_path
+    #     else
+    #       redirect_to request.referrer || root_url
+    #     end
+    #   else
+    #     flash[:danger] = '投稿に失敗しました'
+    #     redirect_to request.referrer || root_url
+    #   end
+    # binding.pry
+    # @post = Note.find(params[:post_id]) #①
   end
 
   def count
